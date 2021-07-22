efe5aa438cd5654a4e843de0b00e02bd241b76f8
SSABOODA <69753846+SSABOODA@users.noreply.github.com>
2021-07-22 15:34:36 +0900

    modify: urls.py 수정

--- api/admins/urls.py
+++ api/admins/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path, include
-# from rest_framework         import routers
-# from rest_framework.routers import DefaultRouter
-
-# router = routers.DefaultRouter()
-# router.register(r'', views.AdminViewSet)
 
 from .views import PublicCostListView, PublicCostDetailView
 
 urlpatterns = [
-    # path('/',include(router.urls)),
+
     path('/', PublicCostListView.as_view()),
     path('/<int:pk>/', PublicCostDetailView.as_view()),
     
 ]
 
-# localhost:8000/api/admins
-# localhost:8000/api/publics
